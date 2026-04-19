40ba531d4c24dba9858f6524ff6d37d187ed7193
kirSm11 <143707974+kirSm11@users.noreply.github.com>
2026-04-19 12:08:39 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // === 🧱 Настройки кэша ===
-const CACHE_NAME = 'den-g-a-v0.102'; // ⬅️ меняй версию при каждом обновлении
+const CACHE_NAME = 'den-g-a-v0.103'; // ⬅️ меняй версию при каждом обновлении
 const urlsToCache = [
   '/',
   '/index.html',
